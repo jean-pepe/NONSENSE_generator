76bb6e38a534d310c4ab806d172041178e33b8e5
sofiaamoress <160346321+sofiaamoress@users.noreply.github.com>
2025-05-12 16:59:41 +0200

    #2 comenti nel codice Noun

--- nonsense-generator/src/main/java/MaGiNiSo/Noun.java
+++ nonsense-generator/src/main/java/MaGiNiSo/Noun.java
@@ -10,31 +10,30 @@ public class Noun {
 	
 	private static List<String> dictionaryNoun;
     private String word;
-	
+
+    //Create the dictionary based on the file 
     public static void setDictionaryNoun() {
     	try {
             dictionaryNoun = Files.readAllLines(Paths.get("Nouns.txt"));
-            
-            if (dictionaryNoun.isEmpty()) {
-                throw new IllegalArgumentException("Empty file");
-            }
-
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
     }
 
+   //This is the method for adding nouns into the dictionary, that we have selected from the input sentence
    public static addNouns(List<String> nouns){
 	   for (String s : nouns){
 		   dictionaryNoun.add(s);
 	 }
    }
-	
+
+    //This method is for creating a new object Noun (basically a word), selecting it randomly from the dictionary
     public Noun() {
     	Random rand = new Random();
         word = dictionaryNoun.get(rand.nextInt(dictionaryNoun.size()));
     }
-    
+
+   //Method for obtaining the noun of the Object
     public String getNoun() {
     	return word;
     }
