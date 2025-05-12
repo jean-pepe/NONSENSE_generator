6dc55ebc9b2fe9398ce190b29f45c51c301bcea1
sofiaamoress <160346321+sofiaamoress@users.noreply.github.com>
2025-05-12 17:01:50 +0200

    #2 comenti nel codice Adjectives

--- nonsense-generator/src/main/java/MaGiNiSo/Adjective.java
+++ nonsense-generator/src/main/java/MaGiNiSo/Adjective.java
@@ -10,31 +10,31 @@ public class Adjective {
 	
 	private static List<String> dictionaryAdj;
     private String word;
-	
+
+    //Method for creating the dictionary of Adjectives
     public static void setDictionaryAdj() {
     	try {
             dictionaryAdj = Files.readAllLines(Paths.get("Adjectives.txt"));
-            
-            if (dictionaryAdj.isEmpty()) {
-                throw new IllegalArgumentException("Empty file");
-            }
 
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
     }
 
+    //Method for adding adjectives from the input sentence to the dictionary
     public static addAdjectives(List<String> adjectives){
 	   for (String s : adjectives){
 		   dictionaryAdj.add(s);
 	 }
      }
-    
+
+    //Method for creating a new object Adjective, with a random word of the dictionary
     public Adjective() {
     	Random rand = new Random();
         word = dictionaryAdj.get(rand.nextInt(dictionaryAdj.size()));
     }
-    
+
+    //Method for getting the word saved in the object
     public String getAdjective() {
     	return word;
     }
