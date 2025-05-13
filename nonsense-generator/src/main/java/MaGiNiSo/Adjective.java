479be634f00cb349da447215f802917c4857f88b
Sofia <sofia.molina.amores@gmail.com>
2025-05-13 05:02:31 +0200

    Prova di abstract class

--- nonsense-generator/src/main/java/MaGiNiSo/Adjective.java
+++ nonsense-generator/src/main/java/MaGiNiSo/Adjective.java
@@ -1,41 +1,18 @@
 package MaGiNiSo;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Random;
 
 public class Adjective {
 	
-	private static List<String> dictionaryAdj;
-    private String word;
+	public static void setDictionary() {
+		Word.createDictionary("src/main/java/MaGiNiSo/Adjectives.txt");
+	}
 
-    //Method for creating the dictionary of Adjectives
-    public static void setDictionaryAdj() {
-    	try {
-            dictionaryAdj = Files.readAllLines(Paths.get("src/main/java/MaGiNiSo/Adjectives.txt"));
-
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-        }
-    }
-
-    //Method for adding adjectives from the input sentence to the dictionary
-    public static void addAdjectives(List<String> adjectives){
-	   for (String s : adjectives){
-		   dictionaryAdj.add(s);
-	 }
-     }
-
-    //Method for creating a new object Adjective, with a random word of the dictionary
-    public Adjective() {
-    	Random rand = new Random();
-        word = dictionaryAdj.get(rand.nextInt(dictionaryAdj.size()));
-    }
-
-    //Method for getting the word saved in the object
-    public String getAdjective() {
-    	return word;
-    }
+	public Adjective() {
+		super();
+	}
+	
+	public static void addAdjectives(List<String> adjectives) {
+		Word.add(adjectives);
+	}
 }
--- nonsense-generator/src/main/java/MaGiNiSo/SentenceStructure.java
+++ nonsense-generator/src/main/java/MaGiNiSo/SentenceStructure.java
@@ -7,30 +7,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class SentenceStructure {
+public class SentenceStructure extends Word{
 	
-	private static List<String> dictionaryStruc;
-    private String randomStructure;
+	public static void setDictionary() {
+		Word.createDictionary("src/main/java/MaGiNiSo/SentenceStructures.txt");
+	}
 
-    //Mehtod for creating the dictionary of Sentence Structures
-    public static void setDictionaryStruc() {
-    	try {
-            dictionaryStruc = Files.readAllLines(Paths.get("src/main/java/MaGiNiSo/SentenceStructures.txt"));
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-        }
-    }
-
-   //Method for creating an object Sentence Structre, selecting a random structure from the dictionary
-    public SentenceStructure() {
-    	Random rand = new Random();
-        randomStructure = dictionaryStruc.get(rand.nextInt(dictionaryStruc.size()));
-    }
-
-    //Method for getting the structure in the object
-    public String getStructure() {
-    	return randomStructure;
-    }
+	public SentenceStructure() {
+		super();
+	}
 	
 }
 
