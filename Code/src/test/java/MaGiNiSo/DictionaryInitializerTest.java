c9703a8cecafb84d29895848fdb1522f3f40fd77
Nicoletta <palagniucnicoletta@gmail.com>
2025-05-27 20:04:09 +0200

    update junit test

--- Code/src/test/java/MaGiNiSo/DictionaryInitializerTest.java
+++ Code/src/test/java/MaGiNiSo/DictionaryInitializerTest.java
@@ -16,15 +16,15 @@ class DictionaryInitializerTest {
     @Test
 	@DisplayName("Dizionari inizializzati non nulli")
     void testDictionaryInitialization() {
-        assertNotNull(dictionaryInitializer.getDictionary("nouns"), "Il dizionario dei sostantivi dovrebbe essere inizializzato");
-        assertNotNull(dictionaryInitializer.getDictionary("verbsPresent"), "Il dizionario dei verbi presenti dovrebbe essere inizializzato");
-        assertNotNull(dictionaryInitializer.getDictionary("verbsPast"), "Il dizionario dei verbi passati dovrebbe essere inizializzato");
-        assertNotNull(dictionaryInitializer.getDictionary("verbsFuture"), "Il dizionario dei verbi futuri dovrebbe essere inizializzato");
-        assertNotNull(dictionaryInitializer.getDictionary("adjectives"), "Il dizionario degli aggettivi dovrebbe essere inizializzato");
+        assertNotNull(dictionaryInitializer.getDictionary("nouns"), "Il dizionario dei sostantivi non inizializzato");
+        assertNotNull(dictionaryInitializer.getDictionary("verbsPresent"), "Il dizionario dei verbi presenti non inizializzato");
+        assertNotNull(dictionaryInitializer.getDictionary("verbsPast"), "Il dizionario dei verbi passati non inizializzato");
+        assertNotNull(dictionaryInitializer.getDictionary("verbsFuture"), "Il dizionario dei verbi futuri non inizializzato");
+        assertNotNull(dictionaryInitializer.getDictionary("adjectives"), "Il dizionario degli aggettivi non inizializzato");
     }
 
     @Test
-	@DisplayName("La richieste di dizionari non esistenti genera un eccezione")
+	@DisplayName("La richieste di dizionari non esistenti genera IllegalArgumentException")
     void testInvalidDictionaryRequest() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             dictionaryInitializer.getDictionary("invalidType");
